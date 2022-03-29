6da53308e0d63c278712c9fa6cb49d31694725be
MCigno94 <mattiacigno27@gmail.com>
2022-03-29 15:11:15 +0200

    initial comit

---
+++ assets-mail/js/main.js
@@ -0,0 +1,13 @@
+/* 
+===> MAIL <===
+Chiedi all’utente la sua email, 
+controlla che sia nella lista di chi può accedere, 
+stampa un messaggio appropriato sull’esito del controllo.
+*/
+
+
+//Chiedere all’utente la sua email
+
+//Controllare che sia nella lista di chi può accedere
+
+//Stampare un messaggio appropriato sull’esito del controllo (allert)
