3d06ff840f16f122a0406ffa376cca10d1452dea
MCigno94 <mattiacigno27@gmail.com>
2022-03-29 16:47:40 +0200

    final commit - email

--- assets-mail/js/main.js
+++ assets-mail/js/main.js
@@ -6,24 +6,40 @@ stampa un messaggio appropriato sull’esito del controllo.
 */
 
 
+
 //Chiedere all’utente la sua email
-let elementEmail = document.getElementById('email');
-let emailInput = elementEmail.value
-let mailService = '@gmail.com';
-let mailList = [`mattia${mailService}`, `paolo${mailService}`, `federica${mailService}`, `anna${mailService}`];
-//console.log(mailList);
+// Variables
+let emailElement = document.getElementById('email');
+let btnElement = document.getElementById('btn');
 
-for (let i = 0; i < mailList.length; i++) {
-    let mailUser = mailList[i];
-    //console.log(mailUser);
+// Array
+const gmailService = '@gmail.com';
+let emailList = [`silvio${gmailService}`, `paolo${gmailService}`, `serena${gmailService}`, `gaia${gmailService}`];
 
-    //Controllare che sia nella lista di chi può accedere
-    if (mailUser == elementEmail) {
-        alert('ok')
+// Function
+btnElement.addEventListener('click', function() {
+    // Variables
+    let emailInput = emailElement.value;
+    let emailVerified = false;
+
+    // Ciclare l'array
+    for (let i = 0; i < emailList.length; i++) {
+        let emailUser = emailList[i];
+
+        // Verificare l'email in input
+        if (emailInput == emailUser) {
+            emailVerified = true;
+        };
+    };
+
+    if (emailVerified == true) {
+        alert('Login avvenuto con successo');
     } else {
-        //alert('mi spiace utente non trovato')
-    }
-}
+        alert('La mail inserita non è corretta, riprova!');
+    };
 
+});
+
+//Controllare che sia n;ella lista di chi può accedere
 
 //Stampare un messaggio appropriato sull’esito del controllo (allert)
