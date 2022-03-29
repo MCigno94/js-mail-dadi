9ded39225b17ce6e69bcae4c31dac8a865ef9b34
MCigno94 <mattiacigno27@gmail.com>
2022-03-29 15:44:49 +0200

    ticket

--- assets-mail/js/main.js
+++ assets-mail/js/main.js
@@ -7,7 +7,23 @@ stampa un messaggio appropriato sull’esito del controllo.
 
 
 //Chiedere all’utente la sua email
+let elementEmail = document.getElementById('email');
+let emailInput = elementEmail.value
+let mailService = '@gmail.com';
+let mailList = [`mattia${mailService}`, `paolo${mailService}`, `federica${mailService}`, `anna${mailService}`];
+//console.log(mailList);
 
-//Controllare che sia nella lista di chi può accedere
+for (let i = 0; i < mailList.length; i++) {
+    let mailUser = mailList[i];
+    //console.log(mailUser);
+
+    //Controllare che sia nella lista di chi può accedere
+    if (mailUser == elementEmail) {
+        alert('ok')
+    } else {
+        //alert('mi spiace utente non trovato')
+    }
+}
+
 
 //Stampare un messaggio appropriato sull’esito del controllo (allert)
