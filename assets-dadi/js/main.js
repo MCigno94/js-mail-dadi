3f958db52a83fcae071d15f3d44a69a22ee1f300
MCigno94 <mattiacigno27@gmail.com>
2022-03-29 17:01:14 +0200

    initial commit - dadi

--- assets-dadi/js/main.js
+++ assets-dadi/js/main.js
@@ -1,18 +1,27 @@
 /* 
-MAIL
-Chiedi all’utente la sua email, 
-controlla che sia nella lista di chi può accedere, 
-stampa un messaggio appropriato sull’esito del controllo.
 GIOCO DEI DADI
 Generare un numero random da 1 a 6, 
 sia per il giocatore sia per il computer. 
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
-// ===> MAIL <===
+// ===> DADI <===
 
-//Chiedere all’utente la sua email
+//Generare un numero random da 1 a 6 per il giocatore
+let numberPlayerElement = Math.floor(Math.random() * 6 + 1);
+console.log(`Numero Player:${numberPlayerElement}`);
+let numberComputerElement = Math.floor(Math.random() * 6 + 1);
+console.log(`Numero PC:${numberComputerElement}`);
 
-//Controllare che sia nella lista di chi può accedere
+if (numberPlayerElement > numberComputerElement) {
+    console.log('hai vinto');
+} else if (numberPlayerElement < numberComputerElement) {
+    console.log('hai perso');
+} else {
+    console.log('pareggio');
+}
 
-//Stampare un messaggio appropriato sull’esito del controllo (allert)
+
+//Generare un numero random da 1 a 6 per il PC
+
+// Stabilire il vincitore, in base a chi fa il punteggio più alto
